6e9a8f2bb7b3122433ef6e7894127b4ac3553a47
aaronlee <aaronlee.uva@gmail.com>
2023-02-14 21:40:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import csv
+import json
+import requests
+
+url = 'https://rest.iad-06.braze.com/subscription/status/set'
+API_KEY = 'Bearer bed00049-fa53-4e0a-b6c5-ea55f5ddc4f2'
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': API_KEY
+}
+
+with open('sms_subscription_group_migration.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        if len(row)==0:
+            continue
+        temp = row[0].split(",")
+        if temp[0]=="external_id":
+            continue
+
+        body = json.dumps({
+            "subscription_group_id": "7726f4af-54bc-49a0-9dba-4a3338788679",
+            "subscription_state": "subscribed",
+            "external_id": temp[0]
+        })
+
+        response = requests.post(url, data=body, headers=headers)
+        print(response.json())
